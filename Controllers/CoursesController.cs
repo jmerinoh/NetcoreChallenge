4144dc2027a01fc6e43fcce5d311faa2f5f24a3f
Jorge Merino <jmerinoh@gmail.com>
2019-02-05 22:16:21 -0700

    1 single line to return all rows as Json
    Nate commented that I could use a single line to return all rows and fields from context into a Json... here we go!

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -13,27 +13,11 @@ namespace NetcoreChallenge.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<Course> Get()
         {
-            string valuex = "{";
             // Gets all courses in database
             using (var context = new LibraryContext())
-            {
-                var courses = context.Course;
-                foreach (var course in courses)
-                {
-                    valuex = valuex + (valuex == "{" ? "" : ",")
-                    + "\"cou" + course.course_id + "\":"
-                    + "{\"name\":" + " \"" + course.name + "\""
-                    + ",\"desc\":" + " \"" + course.desc + "\""
-                    + ",\"price\":" + " " + course.price
-                    + ",\"status\":" + " \"" + course.status + "\""
-                    + "}"
-                    ;
-                }
-            }
-            Console.WriteLine(valuex);
-            return Content(valuex + "}");
+                return context.Course.ToList();
         }
 
     }
